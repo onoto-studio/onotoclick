57acc0a2d25c948d249e57f4a7d8034d547aab59
onoto-studio <128829377+onoto-studio@users.noreply.github.com>
2025-04-29 13:16:31 +0200

    Update index.tsx

--- src/slices/Buttons/index.tsx
+++ src/slices/Buttons/index.tsx
@@ -1,24 +1,62 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { type Content, isFilled } from "@prismicio/client";
+import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
+import type { SliceComponentProps, JSXMapSerializer } from "@prismicio/react";
 
-/**
- * Props for Buttons.
- */
-export type ButtonsProps = SliceComponentProps<Content.ButtonsSlice>;
+import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
+import { PrismicRichText } from "@/components/PrismicRichText";
 
-/**
- * Component for "Buttons" Slices.
- */
-const Buttons: FC<ButtonsProps> = ({ slice }) => {
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <Heading as="h2" size="xl" className="mb-4 mt-12 first:mt-0 last:mb-0">
+      {children}
+    </Heading>
+  ),
+};
+
+type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+const Hero: FC<HeroProps> = ({ slice }) => {
+  const backgroundImage = slice.primary.backgroundImage;
+
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for buttons (variation: {slice.variation}) Slices
+    <section className="bg-white text-slate-900">
+      <Bounded yPadding="lg">
+        <div className="grid grid-cols-9 gap-4 items-center">
+          {/* Image dans les 6 premières colonnes */}
+          {isFilled.image(backgroundImage) && (
+            <div className="col-span-6  pr-6">
+              <PrismicNextImage
+                field={backgroundImage}
+                alt=""
+                className="w-full h-auto object-cover rounded-lg"
+              />
+            </div>
+          )} 
+          {/* Texte dans les 3 dernières colonnes */}
+          <div className="col-span-3 flex flex-col justify-center space-y-6">
+            <div>
+              <PrismicRichText
+                field={slice.primary.text}
+                components={components}
+              />
+            </div>
+            {isFilled.link(slice.primary.buttonLink) && (
+              <PrismicNextLink
+                field={slice.primary.buttonLink}
+                className="inline-block rounded-sm bg-white px-5 py-3 font-medium text-slate-800"
+              >
+                {slice.primary.buttonText || "Learn More"}
+              </PrismicNextLink>
+            )}
+          </div>
+        </div>
+      </Bounded>
     </section>
   );
 };
 
-export default Buttons;
+
+
+export default Hero;
